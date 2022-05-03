b1171c78cdb469ec19c3f3e83bf02bef62b4b757
MaheshSurepalli <surepallimahesh8@gmail.com>
2022-05-03 10:19:47 +0530

    Voting Dapp

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    const Ballot = await ethers.getContractFactory("Ballot");
+    const ballot = await Ballot.deploy("Digital voting", "Will you agree?");
+  
+    console.log("Balance contract address:", ballot.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
